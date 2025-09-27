1493ad3f58d391a23528b8c0cb9263d3aab504dd
Ivonnq07 <qivonn07@gmail.com>
2025-09-26 20:25:13 -0600

    Add files via upload
    Avance 5 - Ivonn

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 # Proyecto: Simulador de Presupuesto Mensual
 
-from typing import get_overloads
-
 def mostrar_bienvenida():
     print("======================================")
     print("   Simulador de Presupuesto Mensual   ")
@@ -9,21 +7,25 @@ def mostrar_bienvenida():
 
 def mostrar_valores(ingresos, deuda):
     print("Ingreso mensual:", ingresos)
-    print("Deuda mensual:", deuda)
+    print("Deuda inicial:", deuda)
 
 
 def calcular_presupuesto(ingresos, deuda):
     gastos = ingresos * 0.50 
     ahorro = ingresos * 0.30  
-    restante = ingresos - gastos - deuda
-    return gastos, ahorro, restante
+    disponible = ingresos - gastos
+    pago_max = ingresos * 0.20
+    pago_deuda = min(disponible, deuda, pago_max)
+    deuda_restante = deuda - pago_deuda
+    restante = disponible - pago_deuda
+    return gastos, ahorro, deuda_restante, restante
 
 
-def mostrar_resumen(gastos, ahorro, deuda, restante):
-     print("--- Resumen ---")
+def mostrar_resumen(mes, gastos, ahorro, deuda, restante):
+     print(f"\n--- Mes {mes} ---")
      print("Gastos:", gastos)
      print("Ahorro:", ahorro)
-     print("Deuda:", deuda)
+     print("Deuda restante:", deuda)
      print("Dinero restante:", restante)
      if restante > 0:
          print("¡Felicidades! Te sobra dinero este mes.")
@@ -35,12 +37,18 @@ def mostrar_resumen(gastos, ahorro, deuda, restante):
 def main():
     mostrar_bienvenida()
 
-    ingresos = float(input("Ingrese su ingreso mensual:"))
-    deuda = float(input(" Ingrese su deuda mensual:"))
+    ingresos = float(input("\nIngrese su ingreso mensual:"))
+    deuda = float(input(" Ingrese su deuda inicial:"))
+    meses = int(input("¿Cuantos meses quieres simular?:"))
 
     mostrar_valores(ingresos, deuda)
-    gastos, ahorro, restante = calcular_presupuesto(ingresos, deuda)
-    mostrar_resumen(gastos, ahorro, deuda, restante)
+
+    for mes in range(1, meses + 1):
+        gastos, ahorro, deuda, restante = calcular_presupuesto(ingresos, deuda)
+        mostrar_resumen(mes, gastos, ahorro, deuda, restante)
+
+    print("\n Fin de la simulacion.")
+
 
 if __name__ == "__main__":
     main()
